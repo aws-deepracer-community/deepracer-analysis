45dce5070721cd85aa3a7ab96654507e70f922c2
Lars Ludvigsen <larsll@outlook.com>
2022-10-02 16:42:15 +0200

    Fix #32

--- Training_analysis.py
+++ Training_analysis.py
@@ -6,7 +6,7 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.13.8
+#       jupytext_version: 1.14.1
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
@@ -114,7 +114,7 @@ model_logs_root = 'logs/sample-console-logs'
 log = DeepRacerLog(model_logs_root)
 
 # load logs into a dataframe
-log.load()
+log.load_robomaker_logs()
 
 try:
     pprint(log.agent_and_network())
