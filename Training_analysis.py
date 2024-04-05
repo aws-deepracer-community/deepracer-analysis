fed6920e53bc1b77665a140a9448c1e0eae95c1d
Surojit Chowdhury <surojit.chowdhury@bp.com>
2024-04-05 17:15:38 +0100

    removed unintended changes

--- Training_analysis.py
+++ Training_analysis.py
@@ -6,7 +6,7 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.16.1
+#       jupytext_version: 1.15.2
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
